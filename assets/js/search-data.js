c927813223254c23ffda132f767a3de03f489914
rameezw <rameezw@gmail.com>
2025-12-21 08:44:07 +0000

    Deploying to gh-pages from @ rameezw/rameezw.github.io@3baf4f8653545c594704fee82e46fe3a80e41884 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,16 +92,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-organized-the-inaugural-cuplv-formal-methods-seminar-and-gave-a-talk-on-verifiaction-of-real-valued-programs",
+          title: 'Organized the inaugural CUPLV Formal Methods Seminar and gave a talk on Verifiaction...',
           description: "",
           section: "News",},{
         id: 'social-cv',
